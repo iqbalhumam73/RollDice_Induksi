1fc2603e8249d7fa749fb190ccf1786819618c45
iqbalhumam73 <iqbalhumam73@gmail.com>
2023-05-17 14:28:26 +0700

    Logic Roll Dice Done

--- app/src/main/java/com/mandiri/induksi_counter/MainActivity.kt
+++ app/src/main/java/com/mandiri/induksi_counter/MainActivity.kt
@@ -2,10 +2,21 @@ package com.mandiri.induksi_counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnRoll = findViewById<Button>(R.id.btn_roll)
+        val tvResult = findViewById<TextView>(R.id.tv_dice_result)
+
+        btnRoll.setOnClickListener(){
+            var result = (1..6).random()
+            tvResult.setText(result.toString())
+        }
     }
 }
